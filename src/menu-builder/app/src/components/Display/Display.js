252c57eaf3ae24ac6193ed24a30a9022f5214584
Jacob Cunningham <jcun4128@gmail.com>
2026-01-31 11:14:24 -0600

    drag drop reposition

--- src/menu-builder/app/src/components/Display/Display.js
+++ src/menu-builder/app/src/components/Display/Display.js
@@ -2,10 +2,23 @@ import { useState } from 'react';
 import './Display.scss';
 
 const Display = (props) => {
-  const [sprites, setSprites] = useState([]);
-  const [movingSprite, setMovingSprite] = useState('');
+  const [sprites, setSprites] = useState({}); // {filename: fileInfo}
 
   const computePosition = (e, spriteName) => {
+    const rect = e.target.getBoundingClientRect();
+    const spriteInfo = sprites[spriteName];
+    const spriteWidth = spriteInfo.width;
+    const spriteHeight = spriteInfo.height;
+
+    setSprites(prevSprites => ({
+      ...prevSprites,
+      [spriteName]: {
+        ...prevSprites[spriteName],
+        top: Math.round(e.clientY - rect.y - (spriteHeight / 2)),
+        left: Math.round(e.clientX - rect.x - (spriteWidth / 2))
+      }
+    }));
+    
     console.log(e, spriteName);
   };
 
@@ -22,41 +35,46 @@ const Display = (props) => {
 
     if ('width' in spriteData) {
       const rect = e.target.getBoundingClientRect();
+      const spriteWidth = spriteData.width;
+      const spriteHeight = spriteData.height;
 
-      setSprites(prevSprites => ([
+      setSprites(prevSprites => ({
         ...prevSprites,
-        {
+        [spriteData.name]: {
           ...spriteData,
-          top: e.clientY - rect.y,
-          left: e.clientX - rect.x
+          top: Math.round(e.clientY - rect.y - (spriteHeight / 2)),
+          left: Math.round(e.clientX - rect.x - (spriteWidth / 2))
         }
-      ]));
+      }));
     }
   };
 
   const renderSprites = () => (
-    sprites.map((sprite, index) => (
-      <img
-        key={index}
-        alt="sprite"
-        className="App__left-display-sprite"
-        src={sprite.data}
-        title={sprite.name}
-        draggable="true"
-        width={sprite.width}
-        height={sprite.height}
-        onDragStart={(e) => {
-          e.dataTransfer.setData('text/plain', JSON.stringify({
-            spriteName: sprite.name
-          }))
-        }}
-        style={{
-          top: sprite.top,
-          left: sprite.left
-        }}
-      />
-    )) 
-  );
+    Object.keys(sprites).map((spriteName, index) => {
+      const sprite = sprites[spriteName];
+
+      return (
+        <img
+          key={index}
+          alt="sprite"
+          className="App__left-display-sprite"
+          src={sprite.data}
+          title={sprite.name}
+          draggable="true"
+          width={sprite.width}
+          height={sprite.height}
+          onDragStart={(e) => {
+            e.dataTransfer.setData('text/plain', JSON.stringify({
+              spriteName: sprite.name
+            }))
+          }}
+          style={{
+            top: sprite.top,
+            left: sprite.left
+          }}
+        />
+      );
+  }));
 
   return (
     <div
@@ -64,7 +82,7 @@ const Display = (props) => {
       onDragOver={(e) => {e.preventDefault()}}
       onDrop={imgDrop}
     >
-      {sprites.length > 0 && renderSprites()}
+      {Object.keys(sprites).length > 0 && renderSprites()}
     </div>
   );
 };
